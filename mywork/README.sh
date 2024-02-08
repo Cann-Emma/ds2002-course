129ed627c75621b5adc9f0fe987f7203ec436ec6
Emmanuella Cann <zgb8ts@virginia.edu>
2024-02-08 14:12:57 -0500

    Add scripting demo files

---
+++ mywork/README.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# Author: Emmanuella Cann
+# Last updated: 2/6/24
+
+read -p " What is your favourite number?" FAVENUM 
+echo "Hey $USERNAME, I heard your favourite number is $FAVENUM"
---
+++ mywork/addme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+read -p " What is the first number you want me to add? "  NUM
+read -p " What is the second number? "  SECNUM
+
+SUMOF=$NUM+$SECNUM
+
+echo "The sum is $SUMOF"
