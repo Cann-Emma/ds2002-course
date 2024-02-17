72b06ce6b0fc17260ff639292651c5abaf17e211
Emmanuella Cann <zgb8ts@virginia.edu>
2024-02-16 21:29:23 -0500

    Adding scripting and environment lab

---
+++ labs/madlib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib"
+
+read -p "1. Please give me a noun: " NOUN1
+read -p "1. Please give me a verb: " VERB1
+read -p "1. Please give me an adverb: " ADV1
+read -p "1. Please give me an adjective: " ADJ1
+read -p "1. Please give me a noun: " NOUN2 
+read -p "1. Please give me an adverb: " ADV2 
+read -p "1. Please give me a verb: "  VERB2
+read -p "1. Please give me an adjective: " ADJ2
+read -p "1. Please give me a noun: " NOUN3
+
+
+echo "Once upon a time, $NOUN1 $VERB1 $ADV1 across $NOUN2."
+echo "It was named the $ADV2 $VERB2 of $ADJ2 $NOUN3."  
