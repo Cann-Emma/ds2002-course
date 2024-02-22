fc7af9c9abe9c9748bd13985c6713885703bf54f
Emmanuella Cann <zgb8ts@virginia.edu>
2024-02-22 18:05:58 -0500

    python scripting for lab3

--- labs/env-vars.py
+++ labs/env-vars.py
@@ -2,18 +2,17 @@
 
 
 import os
+FAVNUM= "4"
+MAJOR= "Psychology"
+YEAR= "third"
 
 # Set variables
-os.environ["FAV_NUM"]="4"
-os.environ["MAJOR"]="Psychology "
-os.environ["YEAR"]="Third"
-
-FAV_NUM=input('What is your favourite number? ')
-MAJOR=input('What is your major? ')
-YEAR=input('What year are you in University? ')
+os.environ["FAV_NUM"]= input('What is your favourite number? ')
+os.environ["MAJOR"]= input('What is your major? ')
+os.environ["YEAR"]= input('What year are you in University? ')
 
 # Fetch variables
 print(os.getenv("FAV_NUM"))
 print(os.getenv("MAJOR"))
-print(os.getenv('UVA_FIRST_YEAR'))
+print(os.getenv('YEAR'))
 
