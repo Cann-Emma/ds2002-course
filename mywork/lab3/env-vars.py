3505d05d413ede7bb37aa6ee78498b992cf5b3d9
Emmanuella Cann <zgb8ts@virginia.edu>
2024-02-29 15:35:19 -0500

    Uploading bash and python scripts for Lab 3

---
+++ mywork/lab3/env-vars.py
@@ -0,0 +1,18 @@
+#!/c/Users/Emmanuella/AppData/Local/Microsoft/WindowsApps/python3
+
+
+import os
+FAVNUM= "4"
+MAJOR= "Psychology"
+YEAR= "third"
+
+# Set variables
+os.environ["FAV_NUM"]= input('What is your favourite number? ')
+os.environ["MAJOR"]= input('What is your major? ')
+os.environ["YEAR"]= input('What year are you in University? ')
+
+# Fetch variables
+print(os.getenv("FAV_NUM"))
+print(os.getenv("MAJOR"))
+print(os.getenv('YEAR'))
+
